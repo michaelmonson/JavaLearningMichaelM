50a84f15e0b621746fd1a531f355225008d21bff
Michael Monson <michaelmonson@gmail.com>
2025-10-05 23:32:42 -0600

    Enhance comments for staircase function
    Added comments to clarify the staircase function's purpose and usage.

--- Code Kata's/Staircase.java
+++ Code Kata's/Staircase.java
@@ -10,11 +10,29 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+/*
+ *
+ */
 class Result {
 
     /*
-     * Complete the 'staircase' function below.
+     * Add staircase function to print pattern
+     * Implement staircase function to print a staircase pattern based on input integer.
      *
+     * Staircase Example:  Here is a staircase of size n = 4:
+     *     #
+     *    ##
+     *   ###
+     *  ####
+     *
+     * Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+     *
+     * Function Description: Write a program that prints a staircase of size n.
+     * 
+     * Complete the staircase function with the following parameter(s):  int 'n' = type integer.
+     * 
+     * Print a staircase as described above. No value should be returned from the method.
+     * Note: The last line is not preceded by spaces. All lines are right-aligned.
      * The function accepts INTEGER n as parameter.
      */
 
@@ -36,9 +54,7 @@ class Result {
             
             System.out.println(spaces + stair);            
         }
-
     }
-
 }
 
 public class Solution {
